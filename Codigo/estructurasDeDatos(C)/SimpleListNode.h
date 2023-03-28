be43a31314cdb4aedd00273dc1832b9c158d97aa
Andres Castro <andres435b@gmail.com>
2023-03-27 23:42:19 -0700

    nuevoNodo function done

--- Codigo/estructurasDeDatos(C)/SimpleListNode.h
+++ Codigo/estructurasDeDatos(C)/SimpleListNode.h
@@ -1,1 +1,51 @@
+#pragma once
+#include <memory.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+
+typedef struct
+{
+    int elemento;
+    struct SimpleListNode* siguienteNodo;
+} SimpleListNode;
+
+typedef struct
+{
+    SimpleListNode* raiz;
+    int longitud;
+
+} SimpleList;
+
+SimpleListNode* nuevoNodo(int valorElemento)
+{
+    SimpleListNode* nuevoNodo = malloc( sizeof(SimpleListNode) );
+    nuevoNodo->elemento = valorElemento;
+    nuevoNodo->siguienteNodo = NULL;
+    return nuevoNodo;
+}
+
+void push_back(SimpleList lista, int valorElemento)
+{
+
+}
+
+void imprimirLista()
+{
+
+}
+
+SimpleListNode* getPtr_predecesorDeElemento(SimpleListNode* raiz, int elemento)
+{
+
+}
+
+void eliminarElemento(int elemento)
+{
+
+}
+
+bool estaVacia()
+{
+
+}
